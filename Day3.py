79981a8d524472721d8ebd944ee2aceecb39b188
Celina <celina.kopiniok@gmx.net>
2022-02-14 10:14:00 +0100

    "Some Changes"

---
+++ Day3.py
@@ -0,0 +1,12 @@
+weekdays = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+]
+print(weekdays[0:3])
+print(weekdays[:2])
+print(weekdays[2:])
---
+++ Exercise8.py
@@ -0,0 +1,10 @@
+trees = ["Birke", "Ahorn", "Kiefer", "Eiche", "Robinie"]
+print(trees)
+print(trees[0:3])
+flowers = ["Maiglöckchen", "Waldveilchen", "Krokus", "Narzisse"]
+print(flowers)
+print(flowers[:2])
+numbers1 = [11, 22, 33, 44, 55, 66, 77, 88]
+numbers2 = [1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31]
+combination = trees + numbers1
+print(combination)
